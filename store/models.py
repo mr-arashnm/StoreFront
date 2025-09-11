fc4bb145ae0a32513950bc9939f8b5da62f54d2d
arash-nm <arashnm797@gmail.com>
2025-09-11 20:54:02 +0330

    Django Authentication System

--- store/models.py
+++ store/models.py
@@ -3,6 +3,8 @@ import re
 from django.db import models
 from uuid import uuid4
 from django.core.validators import MinValueValidator
+from django.conf import settings
+from django.contrib import admin
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -48,22 +50,25 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
     
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-    
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-    
-    class Meta: 
-        db_table = 'store_customer'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
 
     
 class Order(models.Model):
@@ -84,7 +89,12 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id} by {self.customer}"
     
-    
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
+
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
